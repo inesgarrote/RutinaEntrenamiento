70bf16096a37ef002d6a8be2f517ba330d029622
ines <alu0101512297@ull.edu.es>
2025-07-08 11:51:29 +0100

    cambio minSDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 35
     defaultConfig {
         applicationId = "com.example.rutinaentrenamiento"
-        minSdk = 29
+        minSdk = 30
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
